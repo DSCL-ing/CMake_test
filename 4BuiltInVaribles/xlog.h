1d019f54bb937b4ef5465862dc1399aca0269158
DSCLing <102305029+DSCL-ing@users.noreply.github.com>
2024-07-22 23:31:10 +0800

    update

--- 4BuiltInVaribles/xlog.h
+++ 4BuiltInVaribles/xlog.h
@@ -6,10 +6,14 @@
 
 
 #ifdef _WIN32 //WIN32环境
-    #ifdef XLOG_EXPORTS //需要导出(动态库) -- 动态库时,CMake会自动生成xx_EXPORTS变量
-        #define XCPP_API _declspec(dllexport)
-    #else  //否则导入
-        #define XCPP_API _declspec(dllimport)
+    #ifdef xlog_STATIC //如果是静态库 XLOG_STATIC为用户定义
+        #define XCPP_API //不需要导出导入
+    #else
+        #ifdef xlog_EXPORTS //需要导出(动态库) -- 动态库时,CMake会自动生成xx_EXPORTS变量
+            #define XCPP_API _declspec(dllexport)
+        #else  //否则导入
+            #define XCPP_API _declspec(dllimport)
+        #endif
     #endif
 #else  //linux mac环境
     #define XCPP_API //非WIN32环境则为空
