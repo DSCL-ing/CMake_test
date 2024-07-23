6b313df364b4c9fc996a1c5bb37ee86736fded3a
chj <2639075023@qq.com>
2024-07-23 22:13:51 +0800

    output-directory

---
+++ 7directory/xlog/xlog.cc
@@ -0,0 +1,6 @@
+﻿//xlog.cc
+#include<iostream>
+#include"xlog.h"
+Xlog::Xlog(){
+  std::cout<<"Xlog()"<<std::endl;
+}
---
+++ 7directory/xlog/xlog.h
@@ -0,0 +1,23 @@
+﻿//xlog.h
+#ifndef XLOG_H
+#define XLOG_H
+
+#ifdef _WIN32 //WIN32环境
+    #ifdef xlog_STATIC //如果是静态库 XLOG_STATIC为用户定义
+        #define XCPP_API //不需要导出导入
+    #else
+        #ifdef xlog_EXPORTS //需要导出(动态库) -- 动态库时,CMake会自动生成xx_EXPORTS变量
+            #define XCPP_API _declspec(dllexport)
+        #else  //否则导入
+            #define XCPP_API _declspec(dllimport)
+        #endif
+    #endif
+#else  //linux mac环境
+    #define XCPP_API //非WIN32环境则为空
+#endif
+
+class XCPP_API Xlog{
+    public:
+       Xlog();
+};
+#endif
